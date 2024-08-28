6241ac28192644386e23f5b12116927505b44549
JulianKimmig <julian.kimmig@gmx.net>
2024-08-28 16:17:47 +0200

    test imports

---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+from . import testmod
+from .testmod import *  # noqa: F401, F403
+
+
+print(testmod)
--- tests/testmod/__init__.py
+++ tests/testmod/__init__.py
@@ -1,1 +1,4 @@
+from . import testmod  # noqa: F401, F403
 from .testmod import *  # noqa: F401, F403
+
+print(testmod)
