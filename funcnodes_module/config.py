9c4b70fa55eb0b19edf5fed96567c7b4425d0fda
JulianKimmig <julian.kimmig@gmx.net>
2024-08-29 08:54:02 +0200

    cahcing py sometimes leads to errors

--- funcnodes_module/config.py
+++ funcnodes_module/config.py
@@ -4,7 +4,6 @@ template_path = os.path.join(os.path.dirname(__file__), "template_folder")
 files_to_overwrite = [
     os.path.join(".github", "workflows", "py_test.yml"),
     os.path.join(".github", "workflows", "version_publish_main.yml"),
-    os.path.join(".github", "actions", "cache_py", "action.yml"),
     os.path.join(".github", "actions", "install_package", "action.yml"),
     os.path.join("tests", "all_nodes_test_base.py"),
 ]
