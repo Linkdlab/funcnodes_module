3a3cc593d6dda4e0fccff5267a3f80970a36088c
JulianKimmig <julian.kimmig@gmx.net>
2025-01-27 10:04:36 +0100

    updated toml update

--- src/funcnodes_module/update.py
+++ src/funcnodes_module/update.py
@@ -11,6 +11,7 @@ from .config import (
     package_requirements,
 )
 from ._git import _init_git
+from warnings import warn
 
 
 def package_name_version_to_name(name, version):
@@ -84,26 +85,110 @@ def update_toml(path, module_name):
         setuptools["package-dir"] = {"": "src"}
 
     # update remaining poetry [tool.poetry.group.dev.dependencies]
+    python = None
     if "group" in project:
         if "dev" in project["group"]:
             if "dependencies" in project["group"]["dev"]:
                 dev = project["group"]["dev"]["dependencies"]
 
-                if "dependency-groups" not in project:
+                if "dependency-groups" not in tomldata:
                     project["dependency-groups"] = {}
 
                 project["dependency-groups"]["dev"] = [
                     package_name_version_to_name(k, v) for k, v in dev.items()
                 ]
 
+                del project["group"]["dev"]["dependencies"]
+            if len(project["group"]["dev"]) == 0:
+                del project["group"]["dev"]
+        if len(project["group"]) == 0:
+            del project["group"]
+        else:
+            warn(
+                "group is not empty but it is not suppoted by the official pep pyproject toml spec"
+            )
+
     if "dependencies" in project:
         if isinstance(project["dependencies"], dict):
-            project["dependencies"] = [
-                package_name_version_to_name(k, v)
-                for k, v in project["dependencies"].items()
-            ]
+            dep = []
+            for k, v in project["dependencies"].items():
+                vn = package_name_version_to_name(k, v)
+                if k == "python":
+                    python = vn
+                else:
+                    dep.append(vn)
+            project["dependencies"] = dep
 
-    n_dump = toml.dumps(tomldata)
+    if "requires-python" not in project:
+        if python:
+            project["requires-python"] = python[len("python") :]
+        else:
+            project["requires-python"] = ">=3.11"
+
+    if "authors" in project:
+        if isinstance(project["authors"], list):
+            new_authors = []
+            for author in project["authors"]:
+                if isinstance(author, str):
+                    # "Julian Kimmig <julian.kimmig@gmx.net>" to {
+                    # "name": "Julian Kimmig",
+                    # "email": "julian.kimmig@gmx.net"
+                    # }
+
+                    if "<" in author and ">" in author:
+                        name, email = author.split("<")
+                        email = email.strip("<>")
+                        new_authors.append({"name": name.strip(), "email": email})
+                    else:
+                        new_authors.append({"name": author})
+                else:
+                    new_authors.append(author)
+            project["authors"] = new_authors
+
+    # remove old poetry build system
+    if "build-system" in tomldata:
+        if "requires" in tomldata["build-system"]:
+            if "poetry-core" in tomldata["build-system"]["requires"]:
+                del tomldata["build-system"]
+
+    if "build-system" not in tomldata:
+        tomldata["build-system"] = {
+            "requires": ["setuptools>=42"],
+            "build-backend": "setuptools.build_meta",
+        }
+
+    def clean_dict(d):
+        if isinstance(d, dict):
+            d = {k: clean_dict(v) for k, v in d.items()}
+            for k, v in list(d.items()):
+                if v is None:
+                    del d[k]
+            if len(d) == 0:
+                return None
+        if isinstance(d, list):
+            d = [clean_dict(v) for v in d]
+            d = [v for v in d if v is not None]
+            if len(d) == 0:
+                return None
+
+        return d
+
+    tomldata = clean_dict(tomldata)
+
+    toml_order = [
+        "project",
+        "dependency-groups",
+        "tool",
+        "build-system",
+        "entry-points",
+    ]
+
+    ordered_tomldata = {k: tomldata[k] for k in toml_order if k in tomldata}
+    for k in tomldata:
+        if k not in toml_order:
+            ordered_tomldata[k] = tomldata[k]
+
+    n_dump = toml.dumps(ordered_tomldata)
 
     if o_dump != n_dump:
         with open(path, "w") as f:
